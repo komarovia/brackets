d41349ca18e11225ca821630a0923980cfbde461
komarovia <komarovia@gmail.com>
2021-03-03 23:56:45 +0300

    feat: completed the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+let key = bracketsConfig.map(item => item.join(''))
+
+for (let i = 0; i < key.length; i++) {
+    if (str.includes(key[i])) {
+        str = str.replace(key[i], '');
+        i = -1;
+    }
+}
+return str.length === 0 ? true : false;
 }
